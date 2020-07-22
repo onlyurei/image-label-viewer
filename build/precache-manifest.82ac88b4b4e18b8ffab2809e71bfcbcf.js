c1312c1ee2cc3e2134eadbd1a2a3cf7811ca0200
Cheng Fan <fancheng33@hotmail.com>
2020-07-22 00:19:01 -0700

    build: add build folder to svc

---
+++ build/precache-manifest.82ac88b4b4e18b8ffab2809e71bfcbcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0fbfc3bece93961c5e5b7f49024314c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32f650591b322c35e8f0",
+    "url": "/static/js/2.fac94dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fac94dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf24d7ab7055241853b",
+    "url": "/static/js/main.d413d89e.chunk.js"
+  },
+  {
+    "revision": "6a64e32784b02eddc788",
+    "url": "/static/js/runtime-main.9716dab7.js"
+  }
+]);
